4863df3ad276806ce981faab5207f170246ba3c1
laurinpenland <47644935+laurinpenland@users.noreply.github.com>
2020-02-27 14:19:39 -0500

    Update unproccessed_collections.sql

--- archivesspace-duke-scripts/sql/unproccessed_collections.sql
+++ archivesspace-duke-scripts/sql/unproccessed_collections.sql
@@ -9,7 +9,8 @@ FROM
 		LEFT JOIN
 	enumeration_value ON collection_management.processing_status_id = enumeration_value.id
 WHERE
-	enumeration_value.id=61153
+	/*enumeration_value.id 61153 is "unprocessed", 256 is "in_progress", and null is null*/
+	(enumeration_value.id = 61153 or enumeration_value.id is null or enumeration_value.id=256)
     AND repo_id = 2
-	AND accession.accession_date >= '20190101'
-    AND accession.accession_date <= '20191209'
+	AND accession.accession_date >= '20200101'
+    AND accession.accession_date <= '20200227'
