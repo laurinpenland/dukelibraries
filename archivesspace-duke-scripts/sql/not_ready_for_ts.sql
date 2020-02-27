278eede0305b2d5e56333d3b2d462bc7d65dcb6d
laurinpenland <47644935+laurinpenland@users.noreply.github.com>
2020-02-27 14:13:51 -0500

    Update not_ready_for_ts.sql

--- archivesspace-duke-scripts/sql/not_ready_for_ts.sql
+++ archivesspace-duke-scripts/sql/not_ready_for_ts.sql
@@ -1,16 +1,16 @@
 SELECT 
     accession.identifier AS 'Accession Number',
     accession.title AS 'Title',
-    boolean_2 AS 'Not Ready for TS', 
-    enumeration_value.value AS 'Curator'
+	enumeration_value.value AS 'Processing Status'
 FROM
     accession
         LEFT JOIN
-    user_defined ON accession.id = user_defined.accession_id 
+    collection_management ON accession.id = collection_management.accession_id 
 		LEFT JOIN
-	enumeration_value ON user_defined.enum_1_id = enumeration_value.id
+	enumeration_value ON collection_management.processing_status_id = enumeration_value.id
 WHERE
-	boolean_2 = 0
+	/*enumeration_value.id 61153 is "unprocessed", 256 is "in_progress", and null is null*/
+	(enumeration_value.id = 61153 or enumeration_value.id is null or enumeration_value.id=256)
     AND repo_id = 2
-	AND accession.accession_date >= '20190101'
-    AND accession.accession_date <= '20191209';
+	AND accession.accession_date >= '20200101'
+    AND accession.accession_date <= '20200227'
